cf3b2942e27cf61215581d238c979279d8d42623
Alexis S <alexis.small@canada.ca>
2020-03-04 09:20:46 -0500

    initial create table sql

---
+++ sql/create.sql
@@ -0,0 +1,22 @@
+-- Table: public.flyer_item
+
+-- DROP TABLE public.flyer_item;
+
+CREATE TABLE public.flyer_item
+(
+    price_units character varying(100) COLLATE pg_catalog."default",
+    start_date date,
+    sale_price integer,
+    product_name character varying(500) COLLATE pg_catalog."default",
+    merchant character varying(100) COLLATE pg_catalog."default",
+    end_date date,
+    id bigint NOT NULL DEFAULT nextval('flyer_item_id_seq'::regclass),
+    CONSTRAINT pk_id PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.flyer_item
+    OWNER to postgres;
